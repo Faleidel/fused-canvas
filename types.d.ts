a9c5b2b70a42e0b07b291ed2c339cf40376622df
Raphael Mongeau <raphael.mongeau@difuze.com>
2024-05-13 22:51:29 -0400

    Updates types

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,18 @@
 declare module "fused-canvas" {
-    export function createFusedCanvas(): any;
-    export function createFusedBox(): any;
+    export function createFusedCanvas(): (options: {
+        viewport: HTMLElement,
+        canvas: HTMLElement,
+        mouseDown0Action?: string,
+        mouseDown1Action?: string,
+        mouseDown2Action?: string,
+        scrollAction?: string,
+        scrollCtrlAction?: string,
+        fitToScreen?: boolean,
+        yScaling?: number,
+        dagre?: string
+    }) => {
+        container: HTMLElement,
+        view: { panX: number, panY: number, zoom: number },
+        fitToScreen: () => void,
+    };
 }
